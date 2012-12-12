a1eb49b132f7228a38d6797c1785bbde072aef9d
Travis Roberts <traviskroberts@gmail.com>
2012-12-11 20:18:46 -0600

    Added helper spec.

---
+++ spec/helpers/application_helper_spec.rb
@@ -0,0 +1,13 @@
+require 'spec_helper'
+
+describe ApplicationHelper do
+  describe '#formatted_amount' do
+    it 'should return a negative dollar amount if it is a debit' do
+      helper.formatted_amount(123.45, 'debit').should == '-$123.45'
+    end
+
+    it 'should return a dollar amount if it is a credit' do
+      helper.formatted_amount(123.45, 'credit').should == '$123.45'
+    end
+  end
+end
