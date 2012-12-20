d333bf2e2823d8850e6b0dd92693b40348d42161
Travis Roberts <traviskroberts@gmail.com>
2012-12-19 23:20:00 -0600

    Another angle on asset compilation.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -49,7 +49,7 @@ Ledger::Application.configure do
   # config.action_controller.asset_host = "http://assets.example.com"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
-  config.assets.precompile += %w( cerulean.css )
+  # config.assets.precompile += %w( file.js )
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
