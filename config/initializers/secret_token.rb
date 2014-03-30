bce910252695f80ac18be22e3923057dd291de8f
Travis Roberts <traviskroberts@gmail.com>
2014-03-30 18:53:06 -0500

    Moved session secret to env variable.

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Ledger::Application.config.secret_token = '47fa2a6ff11b993403ea27f3d62767fa393dd4d750af8113aa303edc72fb21ba1471ff5e5c20cd7b5b1f78c6f7ae3526b2e1401aacd49ac193721beee9e54219'
+Ledger::Application.config.secret_token = ENV['SECRET_TOKEN']
