dc85de0711c49af47297691137119c97daabc24e
Travis Roberts <traviskroberts@gmail.com>
2015-10-01 20:58:39 -0500

    Removed temp rake task.

--- config/schedule.rb
+++ config/schedule.rb
@@ -5,7 +5,3 @@ set :output, "/var/www/ledger/shared/log/cron.log"
 every :day, at: "1:00am" do
   rake "recurring_transactions:process"
 end
-
-every 1.minute do
-  rake "sanity_check"
-end
--- lib/tasks/sanity_check.rake
+++
@@ -1,3 +0,0 @@
-task sanity_check: :environment do
-  puts "I’m working!" if Rails.env.production?
-end
